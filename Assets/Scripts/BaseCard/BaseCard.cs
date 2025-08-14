9ce34180dcb66c91b031dd34f3d584824978d48f
saesacga <139521874+saesacga@users.noreply.github.com>
2025-08-14 16:45:00 -0500

    Updated project settings and package versions

--- Assets/Scripts/BaseCard/BaseCard.cs
+++ Assets/Scripts/BaseCard/BaseCard.cs
@@ -17,7 +17,7 @@ public abstract class BaseCard : MonoBehaviour
         _frame.color = color switch
         {
             CardType_Color.CardColor.Green => Color.green,
-            CardType_Color.CardColor.Orange => Color.orange,
+            CardType_Color.CardColor.Orange => Color.yellow,
             CardType_Color.CardColor.Red => Color.red,
             _ => throw new System.Exception("Unvalid card color")
         };
--- Assets/Scripts/ReverseCard.cs
+++ Assets/Scripts/ReverseCard.cs
@@ -9,7 +9,7 @@ public class ReverseCard : MonoBehaviour
         GetComponentInChildren<Image>().color = color switch
         {
             CardType_Color.CardColor.Green => Color.green,
-            CardType_Color.CardColor.Orange => Color.orange,
+            CardType_Color.CardColor.Orange => Color.yellow,
             CardType_Color.CardColor.Red => Color.red,
             _ => throw new System.Exception("Unvalid card color")
         };
